0cb308aa4cfd8b3e1f6203333a44ac6f7d7a2c7e
Aditya Patawari <aditya@adityapatawari.com>
2016-01-11 13:28:58 +0530

    add logging module and retries.

--- benchmark.py
+++ benchmark.py
@@ -1,15 +1,23 @@
 import redis
 import threading
 import time
+import logging
+
+FORMAT = '%(asctime)s - %(message)s'
+logging.basicConfig(filename='redis-benchmark.log', level=logging.INFO, format=FORMAT)
 
 def worker(id):
+	r = redis.StrictRedis(host='localhost', port=6379, db=0)
 	while True: # Infinite loop for workers
-		print "Running worker id: %d" % id	
-		r = redis.StrictRedis(host='localhost', port=6379, db=0)
-		r.set('redis-benchmark-worker%d' % id, time.time())
-		r.get('redis-benchmark-worker%d' % id)
-		#r.delete('redis-benchmark-worker%d' % id)
-		time.sleep(1)
+		logging.info("Running worker id: %d" % id)
+		try:
+			r.set('redis-benchmark-worker%d' % id, time.time())
+			r.get('redis-benchmark-worker%d' % id)
+			#r.delete('redis-benchmark-worker%d' % id)
+			time.sleep(1)
+		except:
+			logging.critical("Lost redis. Trying to reconnect.")
+			r = redis.StrictRedis(host='localhost', port=6379, db=0)
 	return
 
 threads = []
